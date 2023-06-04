2d4459b2d944cbcb0ab8036fc34f276a17094e44
ymohamed <ymohamed@sarmady.net>
2023-06-04 17:01:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+const App = () => {
+  const [imageSrc, setImageSrc] = useState('./imgs/default.png');
 
-function App() {
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    if (clientX < windowWidth / 3 && clientY < windowHeight / 3) {
+      setImageSrc('./imgs/up-left.png'); // top left
+    } else if (clientX >= windowWidth / 3 && clientX < windowWidth / 3 * 2 && clientY < windowHeight / 3) {
+      setImageSrc('./imgs/up.png'); // top center
+    } else if (clientX >= windowWidth / 3 * 2 && clientY < windowHeight / 3) {
+      setImageSrc('./imgs/up-right.png'); // top right
+    } else if (clientX < windowWidth / 3 && clientY >= windowHeight / 3 && clientY < windowHeight / 3 * 2) {
+      setImageSrc('./imgs/left.png'); // middle left
+    } else if (clientX >= windowWidth / 3 && clientX < windowWidth / 3 * 2 && clientY >= windowHeight / 3 && clientY < windowHeight / 3 * 2) {
+      setImageSrc('./imgs/right.png'); // middle center
+    } else if (clientX >= windowWidth / 3 * 2 && clientY >= windowHeight / 3 && clientY < windowHeight / 3 * 2) {
+      setImageSrc('./imgs/right.png'); // middle right
+    } else if (clientX < windowWidth / 3 && clientY >= windowHeight / 3 * 2) {
+      setImageSrc('./imgs/down-left.png'); // bottom left
+    } else if (clientX >= windowWidth / 3 && clientX < windowWidth / 3 * 2 && clientY >= windowHeight / 3 * 2) {
+      setImageSrc('./imgs/down.png'); // bottom center
+    } else {
+      setImageSrc('./imgs/down-right.png'); // bottom right
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' onMouseMove={handleMouseMove}>
+      <img src={imageSrc}  alt=''/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
